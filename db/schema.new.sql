674dab4899718147f2da802ed5937ceef1bdf4cc
lsplaver <lsplaver@yahoo.com>
2018-05-21 18:54:16 -0400

    new .sql files with new version of tables

---
+++ db/schema.new.sql
@@ -0,0 +1,45 @@
+drop database if exists node_quiz_app_db;
+
+create database node_quiz_app_db;
+
+use node_quiz_app_db;
+
+# table names are temporary and may change
+# column names are temporary and may change
+create table Topic_1 (
+    id integer not null auto_increment primary key,
+    question varchar(255) not null,
+    answer1 varchar(255) not null,
+    answer2 varchar(255) not null,
+    answer3 varchar(255) not null,
+    answer4 varchar(255) not null,
+    correctAnswer varchar(255) not null
+);
+
+create table Topic_2 (
+    id integer not null auto_increment primary key,
+    question varchar(255) not null,
+    answer1 varchar(255) not null,
+    answer2 varchar(255) not null,
+    answer3 varchar(255) not null,
+    answer4 varchar(255) not null,
+    correctAnswer varchar(255) not null
+);
+
+create table Topic_3 (
+    id integer not null auto_increment primary key,
+    question varchar(255) not null,
+    answer1 varchar(255) not null,
+    answer2 varchar(255) not null,
+    answer3 varchar(255) not null,
+    answer4 varchar(255) not null,
+    correctAnswer varchar(255) not null
+);
+
+create table Result (
+    id integer not null auto_increment primary key,
+    topicName varchar(255) not null,
+    totalCorrectPerTopic integer not null,
+    totalTakenPerTopic integer not null,
+    totalAverageCorrectPerTopic decimal as ((totalCorrectPerTopic * 1.00) % (totalTakenPerTopic * 1.00))
+);
---
+++ db/seeds.new.sql
@@ -0,0 +1,41 @@
+use node_quiz_app_db;
+
+insert into Topic_1 (question, answer1, answer2, answer3, answer4, correctAnswer) values 
+     ("What type of game system is Pathfinder RPG?", "Pen & Paper", "PC", "Figurines", "GURPs", "Pen & Paper"),
+     ("What type of game were the old Fallout games?", "Pen & Paper", "XBox", "ARPG", "RPG", "RPG"),
+     ("What type of game have the newer Fallout games been?", "Pen & Paper", "RPG", "ARPG", "Casual", "ARPG"),
+     ("How many Might and Magic (Not Heroes) games have there been?", "7", "10", "15", "20", "10"),
+     ("What type of game is an ARPG?", "Strategy", "Role-Playing", "Action Role-Playing", "Adventure Role-Playing", "Action Role-Playing"),
+     ("Which game system does the Baldur's Gate series use?", "Pathfinder", "2nd Edition AD&D", "ShadowRun", "GURPs", "2nd Edition AD&D"),
+     ("tempQuestion7", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+     ("tempQuestion8", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+     ("tempQuestion9", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer3"),
+     ("tempQuestion10", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer4");
+
+insert into Topic_2 (question, answer1, answer2, answer3, answer4, correctAnswer) values 
+("tempQuestion1", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion2", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion3", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer3"),
+("tempQuestion4", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer4"),
+("tempQuestion5", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion6", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion7", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion8", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion9", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer3"),
+("tempQuestion10", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer4");
+
+insert into Topic_3 (question, answer1, answer2, answer3, answer4, correctAnswer) values 
+("tempQuestion1", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion2", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion3", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer3"),
+("tempQuestion4", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer4"),
+("tempQuestion5", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion6", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion7", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer1"),
+("tempQuestion8", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer2"),
+("tempQuestion9", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer3"),
+("tempQuestion10", "tempAnswer1", "tempAnswer2", "tempAnswer3", "tempAnswer4", "tempAnswer4");
+
+# insert into Result (topicName, totalCorrectPerTopic, totalTakenPerTopic) values ("Topic 1", 0, 0),("Topic 2", 0, 0),("Topic 3", 0, 0);
+
+# insert into Result (topicName, totalCorrectPerTopic, totalTakenPerTopic, totalAverageCorrectPerTopic) values ("Topic 4", 0, 0, 0.00);
