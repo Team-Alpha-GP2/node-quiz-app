6be1a7f0b1fe2e8371e15c879e072abd210f7881
lsplaver <lsplaver@yahoo.com>
2018-05-21 04:59:42 -0400

    updated views to match master

---
+++ views/testing.js
@@ -0,0 +1,32 @@
+var http = require("https");
+
+var options = {
+  "method": "GET",
+  "hostname": 
+    "opentdb",
+    "com"
+  ,
+  "path": [
+    "api.php"
+  ],
+  "headers": {
+    "Cache-Control": "no-cache",
+    "Postman-Token": "c4c463ca-b231-4d8c-bb72-36ea418eab59"
+  }
+};
+
+var req = http.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+req.end();
+
