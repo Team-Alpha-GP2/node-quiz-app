56bf77bba04e910c6c90bed7a7d319a135974bd7
lsplaver <lsplaver@yahoo.com>
2018-05-21 05:27:56 -0400

    updated server with additional from master version

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-// var sequalize = require("sequalize");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var db = require("./models");
 var PORT = process.env.PORT || 3000;
@@ -7,10 +7,17 @@ var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var exprhbs = require("express-handlebars");
-app.engine("handlebars", exprhbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-require("./controllers/quizController.js")(app);
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+// require("./controllers/quizController.js")(app);
+
+app.get("/quizForm", function(req,res){
+    res.render("quizForm");
+});
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("Currently listening at http://localhost:" + PORT);
